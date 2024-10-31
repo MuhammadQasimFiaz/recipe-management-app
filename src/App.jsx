b5e08a7bb550a7538dfff13a21401a2b038eabb6
MuhammadQasimFiaz <qmughal794@gmail.com>
2024-10-31 23:54:31 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='bg-gray-600'>
+      <div className='text-orange-400'>
+        hello
+      </div>
+    </div>
+  )
+}
+
+export default App
