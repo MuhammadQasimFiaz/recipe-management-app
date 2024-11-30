7eadebe57b623f4b252eaa53d33faa85a748335a
MuhammadQasimFiaz <qmughal794@gmail.com>
2024-11-30 14:47:48 +0500

    solve issues

--- src/components/AddRecipe/AddRecipe.jsx
+++ src/components/AddRecipe/AddRecipe.jsx
@@ -1,36 +1,158 @@
-import { useDispatch } from "react-redux"
-import { addRecipe } from '../../features/recipe/recipeSlice'
-import { useState } from "react"
+import React, { useState } from "react";
+import { addRecipe } from "../../features/recipe/recipeSlice";
+import { useDispatch } from "react-redux";
 
 function AddRecipe() {
-  const [input, setInput] = useState('')
-  const dispatch = useDispatch()
+  const [input, setInput] = useState("");
+  const [inputDescription, setInputDescription] = useState("");
+  const [image, setImage] = useState(null);
+  const [inputServings, setInputServings] = useState(0);
+  const [inputreadyIn, setInputReadyIn] = useState(0);
+  const [inputinstructions, setInputInstructions] = useState("");
+  const [inputingredients, setInputIngredients] = useState("");
+  const [showPopup, setShowPopup] = useState(false)
+  const dispatch = useDispatch();
 
-  const addRecipeHandler = (e) => {
-    e.preventDefault()
-    if(input) {
-      dispatch(addRecipe(input))
-      setInput('')
+  const handleImageChange = (e) => {
+    const file = e.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.addEventListener("load", () => {
+        setImage(reader.result); // Update the state with the Base64 URL
+      });
+      reader.readAsDataURL(file); // Convert the image to a Base64 URL
     }
-  }
+  };
+
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+    if (input) {
+      dispatch(
+        addRecipe({
+          title: input,
+          description: inputDescription,
+          imageUrl: image,
+          servings: inputServings,
+          readyIn: inputreadyIn,
+          instructions: inputinstructions.split("\n"),
+          ingredients: inputingredients.split("\n"),
+        })
+      );
+      setInput("");
+      setInputDescription("");
+      setInputServings("");
+      setInputReadyIn("");
+      setInputInstructions("")
+      setInputIngredients("")
+      setImage(null)
+      setShowPopup(true)
+      setTimeout(() => {
+        setShowPopup(false)
+      }, 3000);
+    }
+  };
 
   return (
-    <form onSubmit={addRecipeHandler} className="space-x-3 mt-12">
-      <input
-        type="text"
-        className="bg-gray-800 rounded border border-gray-700 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
-        placeholder="Enter a Recipe..."
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-      />
-      <button
-        type="submit"
-        className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+    <>
+      <form
+        onSubmit={addTodoHandler}
+        className="flex flex-col space-y-6 w-full mx-auto p-[72px] bg-gray-900 shadow-lg min-h-screen"
       >
-        Add Recipe
-      </button>
-    </form>
-  )
+        <h2 className="text-[45px] font-bold text-[#a16925] mb-8 text-center uppercase font-montserrat">
+          Add a Recipe
+        </h2>
+
+        <div className="w-[50%] mx-auto">
+          <label className="block text-white font-semibold">Title:</label>
+          <input
+            type="text"
+            className="bg-gray-700 rounded border border-gray-600 text-gray-100 py-3 px-4 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-200 ease-in-out placeholder:text-gray-400 w-full mb-5"
+            placeholder="Enter a Recipe Title"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            required
+          />
+          <label className="block text-white font-semibold">Servings:</label>
+          <input
+            type="number"
+            className="bg-gray-700 rounded border border-gray-600 text-gray-100 py-3 px-4 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-200 ease-in-out placeholder:text-gray-400 w-full mb-5"
+            placeholder="Enter number of Servings"
+            value={inputServings}
+            onChange={(e) => setInputServings(e.target.value)}
+            required
+          />
+          <label className="block text-white font-semibold">
+            Ready in minutes:
+          </label>
+          <input
+            type="number"
+            className="bg-gray-700 rounded border border-gray-600 text-gray-100 py-3 px-4 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-200 ease-in-out placeholder:text-gray-400 w-full mb-5"
+            placeholder="Enter Time Ready in minutes"
+            value={inputreadyIn}
+            onChange={(e) => setInputReadyIn(e.target.value)}
+            required
+          />
+          <label className="block text-white font-semibold">Ingredients:</label>
+          <textarea
+            type="text"
+            className="bg-gray-700 rounded border border-gray-600 text-gray-100 py-3 px-4 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-200 ease-in-out placeholder:text-gray-400 w-full mb-5"
+            placeholder="Enter Ingredients in points"
+            value={inputingredients}
+            onChange={(e) => setInputIngredients(e.target.value)}
+            required
+          />
+          <label className="block text-white font-semibold">
+            Instructions:
+          </label>
+          <textarea
+            type="text"
+            className="bg-gray-700 rounded border border-gray-600 text-gray-100 py-3 px-4 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-200 ease-in-out placeholder:text-gray-400 w-full mb-5"
+            placeholder="Enter Instructions in points"
+            value={inputinstructions}
+            onChange={(e) => setInputInstructions(e.target.value)}
+            required
+          />
+          <label className="block text-white font-semibold">Summary:</label>
+          <textarea
+            type="text"
+            className="bg-gray-700 rounded border border-gray-600 text-gray-100 py-3 px-4 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-200 ease-in-out placeholder:text-gray-400 w-full mb-5"
+            placeholder="Enter a Recipe Summary"
+            value={inputDescription}
+            onChange={(e) => setInputDescription(e.target.value)}
+            required
+          />
+          <label className="block text-white font-semibold">Image:</label>
+          <input
+            type="file"
+            accept="image/*"
+            onChange={handleImageChange}
+            className="bg-gray-700 rounded border border-gray-600 text-gray-100 py-3 px-4 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-200 ease-in-out w-full mb-5"
+          />
+
+          {image && (
+            <div className="mt-4">
+              <img
+                src={image}
+                alt="Recipe Preview"
+                className="w-32 h-32 object-cover rounded"
+              />
+            </div>
+          )}
+          <button
+            type="submit"
+            className="bg-[#a16925] text-white  py-3 px-6 rounded-lg shadow hover:bg- transition duration-200 ease-in-out w-full"
+          >
+            Add Recipe
+          </button>
+          {showPopup &&
+            <div className="fixed bottom-4 right-4 bg-green-500 text-white py-2 px-4 rounded shadow-lg">
+              Recipe added successfully!
+            </div>
+          }
+        </div>
+      </form>
+    </>
+  );
 }
 
-export default AddRecipe
+export default AddRecipe;
--- src/features/recipe/recipeSlice.js
+++ src/features/recipe/recipeSlice.js
@@ -1,158 +1,93 @@
-import React, { useState } from "react";
-import { addRecipe } from "../../features/recipe/recipeSlice";
-import { useDispatch } from "react-redux";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
-function AddRecipe() {
-  const [input, setInput] = useState("");
-  const [inputDescription, setInputDescription] = useState("");
-  const [image, setImage] = useState(null);
-  const [inputServings, setInputServings] = useState(0);
-  const [inputreadyIn, setInputReadyIn] = useState(0);
-  const [inputinstructions, setInputInstructions] = useState("");
-  const [inputingredients, setInputIngredients] = useState("");
-  const [showPopup, setShowPopup] = useState(false)
-  const dispatch = useDispatch();
-
-  const handleImageChange = (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.addEventListener("load", () => {
-        setImage(reader.result); // Update the state with the Base64 URL
-      });
-      reader.readAsDataURL(file); // Convert the image to a Base64 URL
+const loadFromLocalStorage = () => {
+  try {
+    const savedRecipes = localStorage.getItem("recipes");
+    if (savedRecipes) {
+      const parsedRecipes = JSON.parse(savedRecipes);
+      console.log('Loaded recipes from localStorage:', parsedRecipes);
+      if (Array.isArray(parsedRecipes)) {
+        return parsedRecipes;
+      }
     }
-  };
+  } catch (e) {
+    console.error("Failed to load recipes from localStorage:", e);
+  }
+  return [];
+};
 
-  const addTodoHandler = (e) => {
-    e.preventDefault();
-    if (input) {
-      dispatch(
-        addRecipe({
-          title: input,
-          description: inputDescription,
-          imageUrl: image,
-          servings: inputServings,
-          readyIn: inputreadyIn,
-          instructions: inputinstructions.split("\n"),
-          ingredients: inputingredients.split("\n"),
-        })
+const saveToLocalStorage = (recipes) => {
+  try {
+    localStorage.setItem("recipes", JSON.stringify(recipes));
+  } catch (e) {
+    console.error("Failed to save recipes to localStorage:", e);
+  }
+};
+
+const initialState = {
+  recipes: loadFromLocalStorage(),
+};
+
+export const counterSlice = createSlice({
+  name: "recipeAdd",
+  initialState,
+  reducers: {
+    addRecipe: (state, action) => {
+      const {
+        title,
+        description,
+        imageUrl,
+        servings,
+        readyIn,
+        instructions,
+        ingredients,
+      } = action.payload;
+      console.log('Adding', { title, description, imageUrl, servings, readyIn, instructions, ingredients })
+      const recipe = {
+        id: nanoid(),
+        title,
+        description,
+        imageUrl,
+        servings,
+        readyIn,
+        instructions,
+        ingredients,
+      };
+      state.recipes.push(recipe);
+      saveToLocalStorage(state.recipes); // Save updated recipes to localStorage
+    },
+    deleteRecipe: (state, action) => {
+      state.recipes = state.recipes.filter(
+        (recipe) => recipe.id !== action.payload
       );
-      setInput("");
-      setInputDescription("");
-      setInputServings("");
-      setInputReadyIn("");
-      setInputInstructions("")
-      setInputIngredients("")
-      setImage(null)
-      setShowPopup(true)
-      setTimeout(() => {
-        setShowPopup(false)
-      }, 3000);
-    }
-  };
+      saveToLocalStorage(state.recipes);
+    },
+    updateRecipe: (state, action) => {
+      const {
+        id,
+        title,
+        description,
+        imageUrl,
+        servings,
+        readyIn,
+        instructions,
+        ingredients,
+      } = action.payload;
+      const recipe = state.recipes.find((recipe) => recipe.id === id);
+      if (recipe) {
+        if (title) recipe.title = title;
+        if (description) recipe.description = description;
+        if (imageUrl) recipe.imageUrl = imageUrl;
+        if (servings) recipe.servings = servings;
+        if (readyIn) recipe.readyIn = readyIn;
+        if (instructions) recipe.instructions = instructions;
+        if (ingredients) recipe.ingredients = ingredients;
+        saveToLocalStorage(state.recipes);
+      }
+    },
+  },
+});
 
-  return (
-    <>
-      <form
-        onSubmit={addTodoHandler}
-        className="flex flex-col space-y-6 w-full mx-auto p-[72px] bg-gray-900 shadow-lg min-h-screen"
-      >
-        <h2 className="text-[45px] font-bold text-[#a16925] mb-8 text-center uppercase font-montserrat">
-          Add a Recipe
-        </h2>
+export const { addRecipe, deleteRecipe, updateRecipe } = counterSlice.actions;
 
-        <div className="w-[50%] mx-auto">
-          <label className="block text-white font-semibold">Title:</label>
-          <input
-            type="text"
-            className="bg-gray-700 rounded border border-gray-600 text-gray-100 py-3 px-4 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-200 ease-in-out placeholder:text-gray-400 w-full mb-5"
-            placeholder="Enter a Recipe Title"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            required
-          />
-          <label className="block text-white font-semibold">Servings:</label>
-          <input
-            type="number"
-            className="bg-gray-700 rounded border border-gray-600 text-gray-100 py-3 px-4 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-200 ease-in-out placeholder:text-gray-400 w-full mb-5"
-            placeholder="Enter number of Servings"
-            value={inputServings}
-            onChange={(e) => setInputServings(e.target.value)}
-            required
-          />
-          <label className="block text-white font-semibold">
-            Ready in minutes:
-          </label>
-          <input
-            type="number"
-            className="bg-gray-700 rounded border border-gray-600 text-gray-100 py-3 px-4 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-200 ease-in-out placeholder:text-gray-400 w-full mb-5"
-            placeholder="Enter Time Ready in minutes"
-            value={inputreadyIn}
-            onChange={(e) => setInputReadyIn(e.target.value)}
-            required
-          />
-          <label className="block text-white font-semibold">Ingredients:</label>
-          <textarea
-            type="text"
-            className="bg-gray-700 rounded border border-gray-600 text-gray-100 py-3 px-4 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-200 ease-in-out placeholder:text-gray-400 w-full mb-5"
-            placeholder="Enter Ingredients in points"
-            value={inputingredients}
-            onChange={(e) => setInputIngredients(e.target.value)}
-            required
-          />
-          <label className="block text-white font-semibold">
-            Instructions:
-          </label>
-          <textarea
-            type="text"
-            className="bg-gray-700 rounded border border-gray-600 text-gray-100 py-3 px-4 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-200 ease-in-out placeholder:text-gray-400 w-full mb-5"
-            placeholder="Enter Instructions in points"
-            value={inputinstructions}
-            onChange={(e) => setInputInstructions(e.target.value)}
-            required
-          />
-          <label className="block text-white font-semibold">Summary:</label>
-          <textarea
-            type="text"
-            className="bg-gray-700 rounded border border-gray-600 text-gray-100 py-3 px-4 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-200 ease-in-out placeholder:text-gray-400 w-full mb-5"
-            placeholder="Enter a Recipe Summary"
-            value={inputDescription}
-            onChange={(e) => setInputDescription(e.target.value)}
-            required
-          />
-          <label className="block text-white font-semibold">Image:</label>
-          <input
-            type="file"
-            accept="image/*"
-            onChange={handleImageChange}
-            className="bg-gray-700 rounded border border-gray-600 text-gray-100 py-3 px-4 focus:ring-2 focus:ring-indigo-500 focus:outline-none transition duration-200 ease-in-out w-full mb-5"
-          />
-
-          {image && (
-            <div className="mt-4">
-              <img
-                src={image}
-                alt="Recipe Preview"
-                className="w-32 h-32 object-cover rounded"
-              />
-            </div>
-          )}
-          <button
-            type="submit"
-            className="bg-[#a16925] text-white  py-3 px-6 rounded-lg shadow hover:bg- transition duration-200 ease-in-out w-full"
-          >
-            Add Recipe
-          </button>
-          {showPopup &&
-            <div className="fixed bottom-4 right-4 bg-green-500 text-white py-2 px-4 rounded shadow-lg">
-              Recipe added successfully!
-            </div>
-          }
-        </div>
-      </form>
-    </>
-  );
-}
-
-export default AddRecipe;
+export default counterSlice.reducer;
